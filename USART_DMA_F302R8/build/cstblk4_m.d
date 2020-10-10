eab0ce3bc91874efbccba486484016729c64cd77
geosmall <geomall92@gmail.com>
2020-10-10 17:27:04 -0400

    Added F302R8 DMA example

---
+++ USART_DMA_F302R8/build/cstblk4_m.d
@@ -0,0 +1,106 @@
+build/cstblk4_m.o: \
+ C:/Users/geoma/Desktop/Scilab/toolboxes/STM32_toolbox_1-1/scicos_blocks/src/c/cstblk4_m.c \
+ ../../toolboxes/STM32_toolbox_1-1/scicos_blocks/includes/scicos_block4.h \
+ USART_DMA_F302R8/Inc/STM32_Config.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ Inc/stm32f3xx_hal_conf.h Inc/main.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302x8.h \
+ Drivers/CMSIS/Include/core_cm4.h Drivers/CMSIS/Include/core_cmInstr.h \
+ Drivers/CMSIS/Include/cmsis_gcc.h Drivers/CMSIS/Include/core_cmFunc.h \
+ Drivers/CMSIS/Include/core_cmSimd.h \
+ Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_usart.h \
+ Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_usart_ex.h \
+ Drivers/CMSIS/Include/arm_math.h Drivers/CMSIS/Include/core_cm4.h \
+ ../../toolboxes/STM32_toolbox_1-1/scicos_blocks/core/includes/machine.h \
+ ../../toolboxes/STM32_toolbox_1-1/scicos_blocks/includes/dynlib_scicos_blocks.h
+
+../../toolboxes/STM32_toolbox_1-1/scicos_blocks/includes/scicos_block4.h:
+
+USART_DMA_F302R8/Inc/STM32_Config.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+Inc/stm32f3xx_hal_conf.h:
+
+Inc/main.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302x8.h:
+
+Drivers/CMSIS/Include/core_cm4.h:
+
+Drivers/CMSIS/Include/core_cmInstr.h:
+
+Drivers/CMSIS/Include/cmsis_gcc.h:
+
+Drivers/CMSIS/Include/core_cmFunc.h:
+
+Drivers/CMSIS/Include/core_cmSimd.h:
+
+Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_usart.h:
+
+Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_usart_ex.h:
+
+Drivers/CMSIS/Include/arm_math.h:
+
+Drivers/CMSIS/Include/core_cm4.h:
+
+../../toolboxes/STM32_toolbox_1-1/scicos_blocks/core/includes/machine.h:
+
+../../toolboxes/STM32_toolbox_1-1/scicos_blocks/includes/dynlib_scicos_blocks.h:
